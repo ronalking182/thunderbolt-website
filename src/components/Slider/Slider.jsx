5dae2f624b1157dacda599458f14511504b7d6ad
ronalking182 <abutankokingdavid@icloud.com>
2023-01-13 15:25:17 +0100

    seo

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -12,12 +12,11 @@ const Slider = ({data}) => {
 
   const nextSlide = () => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
-    console.log("next");
   };
 
   const prevSlide = () => {
     setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
-    console.log("prev");
+
   };
 
   function auto() {
